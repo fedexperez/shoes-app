d24e25b8cfe47747dbc4447f4e2f949b83c77ffa
Federico Perez Morales <federicoperez61@gmail.com>
2022-12-26 09:04:10 -0500

    shoe screen file

--- lib/screens/shoe_screen.dart
+++ lib/screens/shoe_screen.dart
@@ -8,14 +8,26 @@ class ShoeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [
-          CustomAppBar(
+        children: [
+          const CustomAppBar(
             title: 'For You',
           ),
-          ShoeContainer(
-            horizontalMargin: 30,
-            verticalMargin: 20,
+          Expanded(
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                children: const [
+                  ShoeContainer(),
+                  ShoeDescription(
+                    title: 'Nike Air Max 720',
+                    description:
+                        "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
+                  ),
+                ],
+              ),
+            ),
           ),
+          const AddToCartContainer(price: 180),
         ],
       ),
     );
